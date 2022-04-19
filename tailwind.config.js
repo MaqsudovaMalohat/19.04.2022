cca94e4d98ba7ab7be2a2ab3595deb884a76e506
unknown <malohatmaqsudova33@gmail.com>
2022-04-19 18:46:42 +0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {
+      colors: {
+        'main-c': '#100A30',
+        'text-hover-c': '#624DE3'
+      },
+      fontFamily: {
+        'main-f': ['Inter', ...defaultTheme. fontFamily.sans] 
+      },
+      fontSize: {
+        'h-title': ['55px', '64px'],
+        'm-title': ['48px', '56px']
+      },
+    },
+  },
+  plugins: [],
+}
